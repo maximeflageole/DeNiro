c623b247b81893b1785fe0afc969996955c2a1a8
DESKTOP-466EECS\minig <maximeflageole@gmail.com>
2020-09-29 20:29:23 -0400

    [Assets] Adding multiple low poly assets to have more diversity of elements in the game [Code][Gameplay] Adding functional towers which shoot at enemies Displaying range of towers

---
+++ DeNiro/Assets/Scripts/Towers/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    protected float m_damage = 10.0f;
+    [SerializeField]
+    protected float m_speed = 500.0f;
+
+    protected TdEnemy m_target;
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        var enemy = other.GetComponent<TdEnemy>();
+        if (enemy == m_target)
+        {
+            enemy.Damage(m_damage);
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(TdEnemy target)
+    {
+        m_target = target;
+    }
+
+    private void Update()
+    {
+        if (m_target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        var directionalVector = (m_target.transform.position - transform.position).normalized;
+        GetComponent<Rigidbody>().velocity = directionalVector * Time.deltaTime * m_speed;
+    }
+}
---
+++ DeNiro/Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField]
+    protected float m_radius = 50.0f;
+    [SerializeField]
+    protected float m_rateOfFire = 1.0f;
+    [SerializeField]
+    protected GameObject m_projectile;
+    [SerializeField]
+    protected Transform m_radiusTransform;
+
+    protected TdEnemy m_target;
+    protected float m_currentFireTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_radiusTransform.localScale = Vector3.one * m_radius / 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_currentFireTimer += Time.deltaTime;
+        if (m_currentFireTimer > m_rateOfFire)
+        {
+            Debug.Log("ReadyToShoot");
+            if (m_target != null)
+            {
+                Shoot();
+                ResetTimer();
+            }
+        }
+    }
+
+    protected void Shoot()
+    {
+        Debug.Log("Shooting");
+        var projectile = Instantiate(m_projectile).GetComponent<Projectile>();
+        if (projectile != null)
+        {
+            projectile.Launch(m_target);
+        }
+    }
+
+    protected void ResetTimer()
+    {
+        m_currentFireTimer = 0.0f;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (m_target != null)
+        {
+            return;
+        }
+        var enemy = other.GetComponent<TdEnemy>();
+        if (enemy != null)
+        {
+            Debug.Log("Target acquired");
+            m_target = enemy;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var enemy = other.GetComponent<TdEnemy>();
+        if (m_target != null && enemy == m_target)
+        {
+            m_target = null;
+        }
+    }
+}
