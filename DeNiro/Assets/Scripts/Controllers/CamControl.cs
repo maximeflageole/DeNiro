6f849f11177b3c71664428edc1f325c95bd4ed4f
DESKTOP-466EECS\minig <maximeflageole@gmail.com>
2020-10-02 17:08:43 -0400

    [Code][Gameplay] Introducing camera controls, you can move with wasd, rotate with qe and zoom with mouse scroll up and down

---
+++ DeNiro/Assets/Scripts/Controllers/CamControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed;
+    [SerializeField]
+    private float m_zoomSpeed = 1;
+    [SerializeField]
+    private float m_rotationSpeed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExecuteXYMovements();
+        Zoom();
+        ExecuteRotation();
+    }
+
+    private void ExecuteXYMovements()
+    {
+        var position = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            position += Vector3.Normalize(Vector3.Cross(transform.right, Vector3.up));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position += Vector3.Normalize(Vector3.Cross(-transform.right, Vector3.up));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position += Vector3.Normalize(Vector3.Cross(new Vector3(0, Time.deltaTime, 0), -transform.up));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            position += Vector3.Normalize(Vector3.Cross(new Vector3(0, Time.deltaTime, 0), transform.up));
+        }
+        transform.position += Vector3.Normalize(position) * m_speed;
+    }
+
+    private void Zoom()
+    {
+        var zoom = Input.mouseScrollDelta.y;
+        if (zoom != 0)
+        {
+            transform.position += transform.forward * m_zoomSpeed * zoom;
+        }
+    }
+
+    private void ExecuteRotation()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, - m_rotationSpeed * Time.deltaTime, 0, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, m_rotationSpeed * Time.deltaTime, 0, Space.World);
+        }
+    }
+}
---
+++ DeNiro/Assets/Scripts/Grid/GridGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector2Int m_dimensions;
+    [SerializeField]
+    protected GameObject m_tilePrefab;
+    [SerializeField]
+    protected float m_tileSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < m_dimensions.x; i++)
+        {
+            for (int j = 0; j < m_dimensions.y; j++)
+            {
+                var tilePosition = new Vector3(i * m_tileSize, 0, j * m_tileSize);
+                Instantiate(m_tilePrefab, tilePosition, Quaternion.identity, transform);
+            }
+        }
+    }
+}
