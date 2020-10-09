2aaeae735855365dec6f2a319e75810908537def
DESKTOP-466EECS\minig <maximeflageole@gmail.com>
2020-10-09 14:48:57 -0400

    [Code][QoL] Introducing a begin and end to hitboxes on aoe effects Introducing radius control for aoe effects

--- DeNiro/Assets/Scripts/Data/AoEData.cs
+++ DeNiro/Assets/Scripts/Data/AoEData.cs
@@ -3,8 +3,10 @@
 [CreateAssetMenu(fileName = "AoeData", menuName = "Aoe Data")]
 public class AoEData : ScriptableObject
 {
-    public float Radius;
-    public float EffectDuration;
-    public uint Damage;
+    public float Radius = 30.0f;
+    public float EffectDuration = 1.0f;
+    public float DamageTimeStart = 0.0f;
+    public float DamageTimeStop = 0.0f;
+    public uint Damage = 20;
     public GameObject PrefabSpawned;
 }
--- DeNiro/Assets/Scripts/Towers/AoE.cs
+++ DeNiro/Assets/Scripts/Towers/AoE.cs
@@ -2,13 +2,38 @@
 
 public class AoE : MonoBehaviour
 {
+    [SerializeField]
+    private CapsuleCollider m_collider;
+    [SerializeField]
+    private float m_yPosition;
+
     private AoEData m_data;
+    private float m_lifespan;
 
     public void Init(AoEData data)
     {
+        transform.position = new Vector3(transform.position.x, m_yPosition, transform.position.z);
         m_data = data;
+        var scale = m_data.Radius / 100.0f;
+        transform.localScale = new Vector3(scale, scale, scale);
+        m_lifespan = 0.0f;
     }
 
+    private void Update()
+    {
+        m_lifespan += Time.deltaTime;
+        if (m_lifespan > m_data.DamageTimeStop)
+        {
+            m_collider.enabled = false;
+            return;
+        }
+        if (m_lifespan > m_data.DamageTimeStart)
+        {
+            m_collider.enabled = true;
+            return;
+        }
+        m_collider.enabled = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
