30f0521af2a0e98941a96245ebcc4cd1968e6eff
DESKTOP-466EECS\minig <maximeflageole@gmail.com>
2020-11-25 17:52:06 -0500

    [Code][Tools] Maps are now loaded from a MapData file created in the MapEditor

--- DeNiro/Assets/Scripts/Grid/GridGenerator.cs
+++ DeNiro/Assets/Scripts/Grid/GridGenerator.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
     [SerializeField]
-    protected Vector2Int m_dimensions;
+    protected List<TilesMaterials> m_tilesMaterials = new List<TilesMaterials>();
+    [SerializeField]
+    protected MapData m_mapData;
     [SerializeField]
     protected GameObject m_tilePrefab;
     [SerializeField]
@@ -12,13 +15,27 @@ public class GridGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < m_dimensions.x; i++)
+        for (int i = 0; i < m_mapData.XSize; i++)
         {
-            for (int j = 0; j < m_dimensions.y; j++)
+            for (int j = 0; j < m_mapData.YSize; j++)
             {
                 var tilePosition = new Vector3(i * m_tileSize, 0, j * m_tileSize) + transform.position;
-                Instantiate(m_tilePrefab, tilePosition, Quaternion.identity, transform);
+                var meshRenderer = Instantiate(m_tilePrefab, tilePosition, Quaternion.identity, transform).GetComponentInChildren<MeshRenderer>();
+                foreach (var tile in m_tilesMaterials)
+                {
+                    if (tile.TileType == m_mapData.Tiles[i * m_mapData.YSize + j])
+                    {
+                        meshRenderer.material = tile.Material;
+                    }
+                }
             }
         }
     }
+}
+
+[System.Serializable]
+public struct TilesMaterials
+{
+    public TileType TileType;
+    public Material Material;
 }
