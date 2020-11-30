ad22706b4e6885df2e38e5665d152631f30159d9
DESKTOP-466EECS\minig <maximeflageole@gmail.com>
2020-11-30 17:49:33 -0500

    [Code][Visuals] Adding an odd/even material types for the tiles to add some variations to the games looks [Assets] Adding multiple visual materials and other visual assets in the game. Will need to clean a lot of the unused stuff at some point

--- DeNiro/Assets/Scripts/Grid/Tile.cs
+++ DeNiro/Assets/Scripts/Grid/Tile.cs
@@ -3,11 +3,17 @@
 public class Tile : MonoBehaviour
 {
     [SerializeField]
+    protected Material m_oddMaterial;
+    [SerializeField]
+    protected Material m_evenMaterial;
+    [SerializeField]
     protected bool m_canHaveTower = false;
     [SerializeField]
     protected Transform m_towerAnchor;
     [SerializeField]
-    private MeshRenderer m_meshRenderer;
+    protected MeshRenderer m_visualMeshRenderer;
+    [SerializeField]
+    private MeshRenderer m_gameplayMeshRenderer;
     [SerializeField]
     public Vector2Int Coordinates;
     public EDirection Orientation { get; protected set; }
@@ -20,12 +26,24 @@ public class Tile : MonoBehaviour
 
     public void SetEnabled(bool enable)
     {
-        m_meshRenderer.enabled = enable;
+        m_gameplayMeshRenderer.enabled = enable;
     }
 
     public void Init(TileDataTuple tileData, Vector2Int coordinates)
     {
         Orientation = tileData.Direction;
         Coordinates = coordinates;
+        if (m_evenMaterial != null && m_oddMaterial != null)
+        {
+            var sum = coordinates.x + coordinates.y;
+            if (sum % 2 == 1)
+            {
+                m_visualMeshRenderer.material = m_evenMaterial;
+            }
+            else
+            {
+                m_visualMeshRenderer.material = m_oddMaterial;
+            }
+        }
     }
 }
