87441cde1c563ce5059bd3c4478ace77275a53a4
Nixonm2 <maximeflageole@gmail.com>
2020-12-15 01:26:47 -0500

    [Code][UI] Adding a script (advantage wheel) to draw the type charts TODO: Also draw the types advantages and disadvantages

---
+++ DeNiro/Assets/Scripts/UI/VisualComponents/AdvantageWheel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvantageWheel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_iconPrefab;
+
+    [SerializeField] private float m_circleSize;
+    [SerializeField] private CreatureTypesChart m_typeChart;
+
+    void OnValidate()
+    {
+        for (int i = transform.childCount; i > 0; --i)
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        
+        var division = 360 / m_typeChart.TypesData.Count;
+        var count = 0;
+        
+        
+        foreach (var data in m_typeChart.TypesData)
+        {
+            var iconTrans = Instantiate(m_iconPrefab, transform).transform;
+            iconTrans.localPosition = new Vector3(Mathf.Sin((count * division) * Mathf.Deg2Rad) * m_circleSize, Mathf.Cos((count * division) * Mathf.Deg2Rad) * m_circleSize, 0);
+            Debug.Log("Sin " + Mathf.Sin(count * division));
+            Debug.Log("Division " + division);
+            count++;
+            iconTrans.GetComponent<Image>().sprite = data.Sprite;
+        }
+    }
+}
--- DeNiro/Assets/Scripts/Units/CreatureTypesChart.cs
+++ DeNiro/Assets/Scripts/Units/CreatureTypesChart.cs
@@ -7,11 +7,11 @@ public class CreatureTypesChart: ScriptableObject
     public Dictionary<ECreatureType, List<ECreatureType>> TypeAdvantageDictionary = new Dictionary<ECreatureType, List<ECreatureType>>();
     public Dictionary<ECreatureType, List<ECreatureType>> TypeDisdvantageDictionary = new Dictionary<ECreatureType, List<ECreatureType>>();
 
-    [SerializeField]
-    private List<CreatureTypesTuple> TypeChart = new List<CreatureTypesTuple>();
+    public List<CreatureTypesTuple> TypeChart = new List<CreatureTypesTuple>();
 
     public List<ECreatureType> GetTypeAdvantages(ECreatureType originalType) { return TypeAdvantageDictionary[originalType]; }
     public List<ECreatureType> GetTypeDisadvantages(ECreatureType originalType) { return TypeDisdvantageDictionary[originalType]; }
+    public List<TypeData> TypesData = new List<TypeData>();
 
 
     public void OnAwake()
